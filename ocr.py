aac0252d06ffed683b26376ed71d5c3d323e6be4
Archupe <92633149+Archupe@users.noreply.github.com>
2024-01-09 09:56:37 +0100

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,13 +2,12 @@ import cv2
 import numpy as np
 import easyocr
 
-image_path = r'C:\Users\hmeisson\Board.png'
-
+image_path = r'C:\Users\Boulanger\Documents\UTBM\VA50\Picture\plateau.jpg'
 
 image = cv2.imread(image_path)
 image_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-#RGB + Tolérance zones intéressantes
+# RGB + Tolérance zones intéressantes
 valeurs_RGB_cibles = [
     (100, 90, 90),
     (110, 90, 80),
@@ -21,12 +20,12 @@ valeurs_RGB_cibles = [
     (100, 100, 100),
     (20, 20, 20),
     (120, 110, 100),
-    #Valeur à adapter en fonction des tests
+    # Valeur à adapter en fonction des tests
 ]
 
 tolérance = 40
 
-#image vide => stocke pixels correspondants
+# Image vide => stocke pixels correspondants
 resultat = np.zeros_like(image_RGB)
 
 for RGB_cible in valeurs_RGB_cibles:
@@ -43,12 +42,12 @@ texte_liste = reader.readtext(resultat)
 resultat = cv2.imread("contours.jpg")
 couleur_rouge = (0, 0, 255)
 
-#Trace txt
+# Trace txt
 with open("resultat_texte.txt", "w", encoding="utf-8") as fichier_texte:
     for result in texte_liste:
         texte = result[1]
-        #filtrage
-        if texte.isalpha() and texte.isupper():
+        # Filtrage
+        if texte.isalnum() and texte.isupper():
             top_left = result[0][0]
             bottom_right = result[0][2]
             top_left = tuple(map(int, top_left))
@@ -56,7 +55,7 @@ with open("resultat_texte.txt", "w", encoding="utf-8") as fichier_texte:
             cv2.rectangle(resultat, top_left, bottom_right, couleur_rouge, 2)
             fichier_texte.write(texte + "\n")
 
-#sortie
+# Sortie
 cv2.imwrite("ville.jpg", resultat)
 
-print("Le texte reconnu (majuscules uniquement) a été enregistré dans 'resultat_texte.txt'")
+print("(majuscules et chiffres) 'resultat_texte.txt'")
